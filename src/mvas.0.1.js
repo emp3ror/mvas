f0a0079b196864e1cd04e68cc5f7ba96649afe36
mjt <thapa.manish16@gmail.com>
2015-01-28 13:28:06 +0545

    testing objects, creating settext, gettext

--- src/mvas.0.1.js
+++ src/mvas.0.1.js
@@ -1,19 +1,57 @@
 (function(window){
     
     'use strict'; //should have defined variables
-    var myCanvas = function (){
+    var mVas = function (param){
+        // init ==>
+        return new mVas.fn.init(param);
+    }
 
-        this.greet = function(){
-            alert("Hello from the " + name + " myCanvas.");
+    mVas.fn = mVas.prototype = {
+        init : function (param) {
+            this.parent; //stores parent ID
+            return this;
+        },
+        helloNew : function () {
+            console.log("hello from new hello");
+        },
+
+        text : function (txt) {
+            this.txt = txt
+        },
+        //set text to the obj
+        setText : function (txt) {
+            this.txt = txt
+        },
+        getText : function () {
+            return this.txt;
+        },
+        setParent : function (parent) {
+            this.parent = parent;
+        },
+        getParent : function () {
+            return this.parent;
         }
+    }
 
-        this.hello = "smth";
+    //this creates prototype to the init function
+    mVas.fn.init.prototype = mVas.fn;
+    mVas.fn.text.prototype = mVas.fn;
 
-        // return myCanvas;
+    //trying what is extend...
+    mVas.func = mVas.prototype = {
+        text : function () {
+            console.log("hey there");
+        }
+    }
+
+    // store text for regular use
+    mVas.text = function (text) {
+        return new mVas.fn.text(text);
     }
     //define globally if it doesn't already exist
-    if(typeof(mVas) === 'undefined'){
-        window.mvas = window.mVas = new myCanvas();
+    if(typeof(window.mVas) === 'undefined'){
+        window.mvas = window.mVas = mVas;
+        // window.mVas = mVas.prototype;
     }
     else{
         console.log("mvas already defined.");
